95fbd5ecc6ed28fb2cf625dc65377b7a0519cbb0
Greg Cain <codegregcode@gmail.com>
2024-09-22 15:59:49 +0100

    completes 039

--- 038_dict_operations.py
+++ 038_dict_operations.py
@@ -67,7 +67,18 @@ print("Function: count_words_by_length")
 # Since there are two words of length 3, etc.
 
 def count_words_by_length(words):
-  pass
+  number_of_letters_in_words = {}
+
+  for word in words:
+    length = len(word)
+
+    if length not in number_of_letters_in_words:
+      number_of_letters_in_words[length] = 1
+
+    else:
+      number_of_letters_in_words[length] += 1
+
+  return number_of_letters_in_words
 
 check_that_these_are_equal(
   count_words_by_length(["hat", "cat", "I", "bird"]),
